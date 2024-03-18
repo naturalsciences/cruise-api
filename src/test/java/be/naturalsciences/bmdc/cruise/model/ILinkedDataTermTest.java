dec18dd4ad1701f08c14983829b8431fe39b31de
Thomas Vandenberghe <thomas.vdberghe@gmail.com>
2024-03-18 16:40:18 +0100

    add test for urns containing _

--- src/test/java/be/naturalsciences/bmdc/cruise/model/ILinkedDataTermTest.java
+++ src/test/java/be/naturalsciences/bmdc/cruise/model/ILinkedDataTermTest.java
@@ -28,8 +28,10 @@ public class ILinkedDataTermTest {
         testUrl("http://ontologies.ef-ears.eu/ears2/1#dev_15/", "ears:dev::15");
         testUrl("http://ontologies.ef-ears.eu/ears2/1#dev_15", "ears:dev::15");
         testUrl("https://ontologies.ef-ears.eu/ears2/1#dev_15", "ears:dev::15");
-        testUrl("http://ontologies.ef-ears.eu/ears2/1/#11BE_dev_35255390-37ad-11ea-92f9-d6fb90a43b87", "ears:dev:11BE::35255390-37ad-11ea-92f9-d6fb90a43b87");
-        testUrl("http://ontologies.ef-ears.eu/ears2/1/#11BE_dev_35255390-37ad-11ea-92f9-d6fb90a43b87/", "ears:dev:11BE::35255390-37ad-11ea-92f9-d6fb90a43b87");
+        testUrl("http://ontologies.ef-ears.eu/ears2/1/#11BE_dev_35255390-37ad-11ea-92f9-d6fb90a43b87",
+                "ears:dev:11BE::35255390-37ad-11ea-92f9-d6fb90a43b87");
+        testUrl("http://ontologies.ef-ears.eu/ears2/1/#11BE_dev_35255390-37ad-11ea-92f9-d6fb90a43b87/",
+                "ears:dev:11BE::35255390-37ad-11ea-92f9-d6fb90a43b87");
         testUrl("https://vocab.nerc.ac.uk/collection/P01/current/SPWGXX01", "SDN:P01::SPWGXX01");
         testUrl("https://vocab.nerc.ac.uk/collection/P01/current/SPWGXX01/", "SDN:P01::SPWGXX01");
         testUrl("https://www.seadatanet.org/urnurl/SDN:P01::SPWGXX01", "SDN:P01::SPWGXX01");
@@ -38,8 +40,16 @@ public class ILinkedDataTermTest {
         testUrl("https://www.seadatanet.org/urnurl/SDN:P01::blabla", "SDN:P01::blabla");
         testUrl("http://ontologies.ef-ears.eu/ears2/1/11BE#dev_1927", "ears:dev:11BE::1927");
         testUrl("http://ontologies.ef-ears.eu/ears2/1/12AE#dev_1927", null);
-        testUrl("http://ontologies.ef-ears.eu/ears2/1/#dev_ff470414-4958-495a-ace9-ff6d6590a4f3","ears:dev::ff470414-4958-495a-ace9-ff6d6590a4f3");
-        testUrl("http://ontologies.ef-ears.eu/ears2/1/#concept_ff470414-4958-495a-ace9-ff6d6590a4f3","ears:concept::ff470414-4958-495a-ace9-ff6d6590a4f3");
+        testUrl("http://ontologies.ef-ears.eu/ears2/1/#dev_ff470414-4958-495a-ace9-ff6d6590a4f3",
+                "ears:dev::ff470414-4958-495a-ace9-ff6d6590a4f3");
+        testUrl("http://ontologies.ef-ears.eu/ears2/1/#concept_ff470414-4958-495a-ace9-ff6d6590a4f3",
+                "ears:concept::ff470414-4958-495a-ace9-ff6d6590a4f3");
+
+        testUrl("http://ontologies.ef-ears.eu/ears2/1#pro_3",
+                "ears:pro::3");
+        testUrl("http://ontologies.ef-ears.eu/ears2/1#pro_3_johnnyTool_All",
+                "ears:pro::3_johnnyTool_All");
+
     }
 
     /**
